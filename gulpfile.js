30414245f664c919ec6abbaaf599a7b45035ba86
Bradley Staples <me@bradleystapl.es>
2015-05-06 01:12:05 -0700

    copy old version files in
    - add coffeescripts, javascripts, images from old version
    - rename public dir to be dist
    - move sass, views, routes into lib
    - added several tasks to gulp file related to new file types
    - not yet working at all

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var autoprefixer = require('gulp-autoprefixer'),
+	coffee = require('gulp-coffee'),
+	coffeelint = require('gulp-coffeelint'),
 	gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	jshint = require('gulp-jshint'),
 	minifycss = require('gulp-minify-css'),
 	nodemon = require('gulp-nodemon'),
 	rename = require('gulp-rename'),
 	sass = require('gulp-sass'),
-	tinylr = require('tiny-lr')();
+	tinylr = require('tiny-lr')(),
+	uglify = require('gulp-uglify');
 
-gulp.task('livereload', function() {
+
+gulp.task('livereload', function () {
 	tinylr.listen(3001); // ./bin/www uses 3000
 });
 
@@ -29,22 +35,54 @@ gulp.task('start', function () {
 	});
 });
 
-gulp.task('styles', function() {
-	return gulp.src('./sass/resizive.scss')
+gulp.task('lintcoffee', function () {
+    gulp.src('./lib/coffee/*.coffee')
+        .pipe(coffeelint())
+        .pipe(coffeelint.reporter())
+});
+
+gulp.task('lintjs', function () {
+ 	gulp.src('./lib/js/*.js')
+    	.pipe(jshint())
+    	.pipe(jshint.reporter('default'));
+});
+
+gulp.task('coffee', function () {
+	// not working atm
+	gulp.src('./lib/coffee/*.coffee')
+    	.pipe(coffee({bare: true})
+    		.on('error', gutil.log))
+    	.pipe(gulp.dest('./dist/javascripts'))
+});
+
+gulp.task('js', function () {
+	gulp.src('./lib/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist/javascripts'));
+});
+
+gulp.task('styles', function () {
+	gulp.src('./lib/sass/resizive.scss')
 		.pipe(sass({style: 'expanded'}))
 		.pipe(autoprefixer('last 2 version'))
-		.pipe(gulp.dest('./public/stylesheets'))
+		.pipe(gulp.dest('./dist/stylesheets'))
 		.pipe(rename({suffix: '.min'}))
 		.pipe(minifycss())
-		.pipe(gulp.dest('./public/stylesheets'));
+		.pipe(gulp.dest('./dist/stylesheets'));
 });
 
-gulp.task('watch', function() {
-	gulp.watch('sass/*.scss', ['styles']);
-	gulp.watch('views/*.jade', notifyLiveReload);
-	gulp.watch('public/stylesheets/*.css', notifyLiveReload);
+gulp.task('watch', function () {
+	gulp.watch('./lib/sass/*.scss', ['styles']);
+	gulp.watch('./lib/js/*.js', ['lintjs']);
+	gulp.watch('./lib/coffee/*.coffee', ['lintcoffee']);
+	gulp.watch('./lib/js/*.js', notifyLiveReload);
+	gulp.watch('./lib/views/*.jade', notifyLiveReload);
+	gulp.watch('dist/stylesheets/*.css', notifyLiveReload);
 });
 
-gulp.task('default', ['styles', 'start', 'livereload', 'watch'], function() {
+
+// to do: image compression
+
+gulp.task('default', ['styles', 'js', 'coffee', 'start', 'livereload', 'watch'], function () {
 
 });
