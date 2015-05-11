873da3c015aae835a13d4b1f0cfac9f1bc599aad
Bradley Staples <the.bradley.staples@gmail.com>
2015-05-10 18:32:45 -0700

    alphabetize gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var autoprefixer = require('gulp-autoprefixer'),
 	coffeelint = require('gulp-coffeelint'),
 	gulp = require('gulp'),
 	gutil = require('gulp-util'),
+	jade = require('gulp-jade'),
 	jshint = require('gulp-jshint'),
 	minifycss = require('gulp-minify-css'),
 	nodemon = require('gulp-nodemon'),
@@ -11,10 +12,6 @@ var autoprefixer = require('gulp-autoprefixer'),
 	tinylr = require('tiny-lr')(),
 	uglify = require('gulp-uglify');
 
-
-gulp.task('livereload', function () {
-	tinylr.listen(3001); // ./bin/www uses 3000
-});
 
 var notifyLiveReload = function (event) {
 	var fileName = require('path').relative(__dirname, event.path);
@@ -26,13 +23,24 @@ var notifyLiveReload = function (event) {
 	});
 }
 
-gulp.task('start', function () {
-	nodemon({
-		script: './bin/www',
-		env: {
-			'NODE_ENV': 'development'
-		}
-	});
+gulp.task('coffee', function () {
+	// not working atm
+	gulp.src('./lib/coffee/*.coffee')
+    	.pipe(coffee({bare: true})
+    		.on('error', gutil.log))
+    	.pipe(gulp.dest('./dist/javascripts'))
+});
+
+gulp.task('html', function() {
+	gulp.src('./lib/views/*.jade')
+		.pipe(jade({pretty: true}))
+		.pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('js', function () {
+	gulp.src('./lib/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist/javascripts'));
 });
 
 gulp.task('lintcoffee', function () {
@@ -47,18 +55,17 @@ gulp.task('lintjs', function () {
     	.pipe(jshint.reporter('default'));
 });
 
-gulp.task('coffee', function () {
-	// not working atm
-	gulp.src('./lib/coffee/*.coffee')
-    	.pipe(coffee({bare: true})
-    		.on('error', gutil.log))
-    	.pipe(gulp.dest('./dist/javascripts'))
+gulp.task('livereload', function () {
+	tinylr.listen(3001); // ./bin/www uses 3000
 });
 
-gulp.task('js', function () {
-	gulp.src('./lib/js/*.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('./dist/javascripts'));
+gulp.task('start', function () {
+	nodemon({
+		script: './bin/www',
+		env: {
+			'NODE_ENV': 'development'
+		}
+	});
 });
 
 gulp.task('styles', function () {
@@ -66,6 +73,12 @@ gulp.task('styles', function () {
 		.pipe(sass({style: 'expanded'}))
 		.pipe(autoprefixer('last 2 version'))
 		.pipe(gulp.dest('./dist/stylesheets'))
+});
+
+gulp.task('styles-min', function () {
+	gulp.src('./lib/sass/resizive.scss')
+		.pipe(sass({style: 'compressed'}))
+		.pipe(autoprefixer('last 2 version'))
 		.pipe(rename({suffix: '.min'}))
 		.pipe(minifycss())
 		.pipe(gulp.dest('./dist/stylesheets'));
@@ -77,12 +90,15 @@ gulp.task('watch', function () {
 	gulp.watch('./lib/coffee/*.coffee', ['lintcoffee']);
 	gulp.watch('./lib/js/*.js', notifyLiveReload);
 	gulp.watch('./lib/views/*.jade', notifyLiveReload);
-	gulp.watch('dist/stylesheets/*.css', notifyLiveReload);
+	gulp.watch('./dist/stylesheets/*.css', notifyLiveReload);
 });
 
-
-// to do: image compression
+// to do: image compression, html rendering in prod, jslin
 
 gulp.task('default', ['styles', 'js', 'coffee', 'start', 'livereload', 'watch'], function () {
+
+});
+
+gulp.task('build', ['styles-min', 'html', 'lintjs', 'js', 'lintcoffee', 'coffee'], function () {
 
 });
