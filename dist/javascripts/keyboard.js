7c3d0cae192c4ce1af0c06a5b083d53b3a87fc34
Bradley Staples <the.bradley.staples@gmail.com>
2015-05-10 18:34:53 -0700

    first run of rendering JS > html

--- dist/javascripts/keyboard.js
+++ dist/javascripts/keyboard.js
@@ -1,1 +1,188 @@
-(function(){var t,n=function(t,n){return function(){return t.apply(n,arguments)}},i=[].indexOf||function(t){for(var n=0,i=this.length;i>n;n++)if(n in this&&this[n]===t)return n;return-1};t=function(){function t(){this.unbindAll=n(this.unbindAll,this),this.unbind=n(this.unbind,this),this.bind=n(this.bind,this)}return t.prototype.body=document.body,t.prototype.keys={left:37,up:38,right:39,down:40,e:69,p:80,r:82,s:83,"+":107,"-":109,"?":191},t.prototype.codes={},t.prototype.methods=["keydown","keyup","keypress"],t.prototype.bindings={keydown:{},keyup:{},keypress:{}},t.prototype.isArray=function(t){return Array.isArray||function(t){return{}.toString.call("[object Array]"===t)}},t.prototype.generateCodes=function(){var t,n,i,e;i=this.keys,e=[];for(n in i)t=i[n],e.push(this.mapCode(n,t));return e},t.prototype.mapCode=function(t,n){return this.codes[n]=t},t.prototype.mapper=function(t,n){var i,e,r,o;return i=n.keyCode,o=n.target.tagName.toLowerCase(),"input"===o||"textarea"===o?!1:i in this.codes?(r=this.codes[i],e=this.bindings[t][r](),this.doesBindingExist(r,t)?e():void 0):!1},t.prototype.sanitizeMethod=function(t){return i.call(this.methods,t)>=0?t:"keydown"},t.prototype.sanitizeKeys=function(t){var n,i,e;return e=function(){var e,r,o;for(o=[],n=e=0,r=t.length;r>e;n=++e)i=t[n],o.push(function(t){return function(n){return n in t.keys?n:void 0}}(this)(i));return o}.call(this)},t.prototype.doesBindingExist=function(t,n){return n in this.bindings?t in this.bindings[n]:!1},t.prototype.register=function(t,n,i){return this.bindings[n][t]=i,document.addEventListener?this.body.addEventListener(n,this.mapper.bind(this,n),!1):this.body.addEventListener("on"+n,this.mapper.bind(this,n)),this.bindings[n][t]},t.prototype.unregister=function(t,n){return document.removeEventListener?this.body.removeEventListener(n,this.mapper.bind(this,n),!1):this.body.detachEvent(n,this.mapper.bind(this,n)),delete this.bindings[n][t]},t.prototype.unbindMethod=function(t){var n,i;i=[];for(n in this.keys)i.push(unbind(n,t));return i},t.prototype.bind=function(t,n,i){var e,r,o,s,d;for(this.isArray(t)||(t=[t]),t=this.sanitizeKeys(t),n=this.sanitizeMethod(n),d=[],e=o=0,s=t.length;s>o;e=++o)r=t[e],d.push(function(t){return function(e){return t.doesBindingExist(e,n)?void 0:t.register(e,n,i)}}(this)(r));return d},t.prototype.unbind=function(t,n){return t in this.keys?(n=this.sanitizeMethod(n),this.doesBindingExist(t,n)?this.unregister(t,n):void 0):!1},t.prototype.unbindAll=function(){var t,n,i,e,r;for(e=this.methods,r=[],n=0,i=e.length;i>n;n++)t=e[n],r.push(this.unbindMethod(t));return r},t}(),$(function(){return window.Keyboard=new t,window.Keyboard.generateCodes()})}).call(this);
+var Keyboard,
+  bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+  indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
+Keyboard = (function() {
+  function Keyboard() {
+    this.unbindAll = bind(this.unbindAll, this);
+    this.unbind = bind(this.unbind, this);
+    this.bind = bind(this.bind, this);
+  }
+
+  Keyboard.prototype.body = document.body;
+
+  Keyboard.prototype.keys = {
+    left: 37,
+    up: 38,
+    right: 39,
+    down: 40,
+    e: 69,
+    p: 80,
+    r: 82,
+    s: 83,
+    '+': 107,
+    '-': 109,
+    '?': 191
+  };
+
+  Keyboard.prototype.codes = {};
+
+  Keyboard.prototype.methods = ['keydown', 'keyup', 'keypress'];
+
+  Keyboard.prototype.bindings = {
+    'keydown': {},
+    'keyup': {},
+    'keypress': {}
+  };
+
+  Keyboard.prototype.isArray = function(value) {
+    return Array.isArray || function(value) {
+      return {}.toString.call(value === '[object Array]');
+    };
+  };
+
+  Keyboard.prototype.generateCodes = function() {
+    var code, key, ref, results;
+    ref = this.keys;
+    results = [];
+    for (key in ref) {
+      code = ref[key];
+      results.push(this.mapCode(key, code));
+    }
+    return results;
+  };
+
+  Keyboard.prototype.mapCode = function(key, code) {
+    return this.codes[code] = key;
+  };
+
+  Keyboard.prototype.mapper = function(method, event) {
+    var code, func, key, tag;
+    code = event.keyCode;
+    tag = event.target.tagName.toLowerCase();
+    if (tag === 'input' || tag === 'textarea') {
+      return false;
+    }
+    if (!(code in this.codes)) {
+      return false;
+    }
+    key = this.codes[code];
+    func = this.bindings[method][key]();
+    if (this.doesBindingExist(key, method)) {
+      return func();
+    }
+  };
+
+  Keyboard.prototype.sanitizeMethod = function(method) {
+    if (indexOf.call(this.methods, method) >= 0) {
+      return method;
+    } else {
+      return 'keydown';
+    }
+  };
+
+  Keyboard.prototype.sanitizeKeys = function(keys) {
+    var index, key, validKeys;
+    return validKeys = (function() {
+      var i, len, results;
+      results = [];
+      for (index = i = 0, len = keys.length; i < len; index = ++i) {
+        key = keys[index];
+        results.push((function(_this) {
+          return function(key) {
+            if (key in _this.keys) {
+              return key;
+            }
+          };
+        })(this)(key));
+      }
+      return results;
+    }).call(this);
+  };
+
+  Keyboard.prototype.doesBindingExist = function(key, method) {
+    if (!(method in this.bindings)) {
+      return false;
+    }
+    return key in this.bindings[method];
+  };
+
+  Keyboard.prototype.register = function(key, method, func) {
+    this.bindings[method][key] = func;
+    if (document.addEventListener) {
+      this.body.addEventListener(method, this.mapper.bind(this, method), false);
+    } else {
+      this.body.addEventListener('on' + method, this.mapper.bind(this, method));
+    }
+    return this.bindings[method][key];
+  };
+
+  Keyboard.prototype.unregister = function(key, method) {
+    if (document.removeEventListener) {
+      this.body.removeEventListener(method, this.mapper.bind(this, method), false);
+    } else {
+      this.body.detachEvent(method, this.mapper.bind(this, method));
+    }
+    return delete this.bindings[method][key];
+  };
+
+  Keyboard.prototype.unbindMethod = function(method) {
+    var key, results;
+    results = [];
+    for (key in this.keys) {
+      results.push(unbind(key, method));
+    }
+    return results;
+  };
+
+  Keyboard.prototype.bind = function(keys, method, func) {
+    var i, index, key, len, results;
+    if (!this.isArray(keys)) {
+      keys = [keys];
+    }
+    keys = this.sanitizeKeys(keys);
+    method = this.sanitizeMethod(method);
+    results = [];
+    for (index = i = 0, len = keys.length; i < len; index = ++i) {
+      key = keys[index];
+      results.push((function(_this) {
+        return function(key) {
+          if (!_this.doesBindingExist(key, method)) {
+            return _this.register(key, method, func);
+          }
+        };
+      })(this)(key));
+    }
+    return results;
+  };
+
+  Keyboard.prototype.unbind = function(key, method) {
+    if (!(key in this.keys)) {
+      return false;
+    }
+    method = this.sanitizeMethod(method);
+    if (!this.doesBindingExist(key, method)) {
+      return;
+    }
+    return this.unregister(key, method);
+  };
+
+  Keyboard.prototype.unbindAll = function() {
+    var i, len, method, ref, results;
+    ref = this.methods;
+    results = [];
+    for (i = 0, len = ref.length; i < len; i++) {
+      method = ref[i];
+      results.push(this.unbindMethod(method));
+    }
+    return results;
+  };
+
+  return Keyboard;
+
+})();
+
+$(function() {
+  window.Keyboard = new Keyboard();
+  return window.Keyboard.generateCodes();
+});
