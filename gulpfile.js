8ea46c05810b8b4bb6d075ecaa65dfb942a3d037
Bradley Staples <the.bradley.staples@gmail.com>
2015-05-13 23:54:48 -0700

    more jshint & jssc config/linting
    - created real jscsrc file
    - added jshintigore file
    - tweaked jshintrc file
    - added favicon back, accidentally deleted previously
    - linted & style-linted js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,75 @@
 'use strict';
 
-var autoprefixer = require('gulp-autoprefixer'),
-	clean = require('gulp-clean'),
-	coffee = require('gulp-coffee'),
-	coffeelint = require('gulp-coffeelint'),
-	concat = require('gulp-concat'),
-	gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	jade = require('gulp-jade'),
-	jscs = require('gulp-jscs'),
-	jshint = require('gulp-jshint'),
-	minifycss = require('gulp-minify-css'),
-	nodemon = require('gulp-nodemon'),
-	rename = require('gulp-rename'),
-	sass = require('gulp-sass'),
-	sourcemaps = require('gulp-sourcemaps'),
-	svgo = require('gulp-svgo'),
-	tinylr = require('tiny-lr')(), 
-	uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
+var coffee = require('gulp-coffee');
+var coffeelint = require('gulp-coffeelint');
+var concat = require('gulp-concat');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var jade = require('gulp-jade');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+var minifycss = require('gulp-minify-css');
+var nodemon = require('gulp-nodemon');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var svgo = require('gulp-svgo');
+var tinylr = require('tiny-lr')();
+var uglify = require('gulp-uglify');
 
 
 var notifyLiveReload = function (event) {
-	var fileName = require('path').relative(__dirname, event.path);
+    var fileName = require('path').relative(__dirname, event.path);
 
-	tinylr.changed({
-		body: {
-			files: [fileName]
-		}
-	});
+    tinylr.changed({
+        body: {
+            files: [fileName]
+        }
+    });
 };
 
 
 gulp.task('clean-files', function () {
-    return gulp.src('dist/stylesheets/resizive.css', {read: false})
+    return gulp.src('dist/stylesheets/resizive.css', {
+            read: false
+        })
         .pipe(clean());
 });
 
 
 gulp.task('coffee-to-javascript', function () {
-	gulp.src('./lib/coffee/*.coffee')
-		.pipe(sourcemaps.init())
-		.pipe(coffee({bare: true})).on('error', gutil.log)
-		.pipe(sourcemaps.write('./maps'))
-    	.pipe(gulp.dest('./dist/javascripts'));
+    gulp.src('./lib/coffee/*.coffee')
+        .pipe(sourcemaps.init())
+        .pipe(coffee({
+            bare: true
+        })).on('error', gutil.log)
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist/javascripts'));
 });
 
 
 gulp.task('combine-and-minify-js', ['move-js-to-dist'], function () {
-	gulp.src('./dist/javascripts/*.js')
-    	.pipe(uglify())
-    	.pipe(concat('resizive.js'))
-    	.pipe(gulp.dest('./dist/javascripts'));
+    gulp.src('./dist/javascripts/*.js')
+        .pipe(uglify())
+        .pipe(concat('resizive.js'))
+        .pipe(gulp.dest('./dist/javascripts'));
 });
 
 
-gulp.task('jade-to-html', function() {
-	gulp.src('./lib/views/*.jade')
-		.pipe(jade({pretty: true}))
-		.pipe(gulp.dest('./dist/'));
+gulp.task('copy-favicon', function () {
+    gulp.src('./lib/views/favicon.ico')
+        .pipe(gulp.dest('./dist/'));
+});
+
+
+gulp.task('jade-to-html', function () {
+    gulp.src('./lib/views/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./dist/'));
 });
 
 
@@ -69,9 +81,9 @@ gulp.task('lint-coffee', function () {
 
 
 gulp.task('lint-js', function () {
- 	gulp.src('./lib/js/*.js')
-    	.pipe(jshint())
-    	.pipe(jshint.reporter('default'));
+    gulp.src(['./lib/js/*.js', 'bin/www'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
 
@@ -82,15 +94,17 @@ gulp.task('lint-js-styles', function () {
 
 
 gulp.task('live-reload', function () {
-	tinylr.listen(3001); // ./bin/www uses 3000
+    tinylr.listen(3001); // ./bin/www uses 3000
 });
 
 
 gulp.task('minifiy-css', ['sass-to-css', 'prefix-css'], function () {
-	gulp.src('./dist/stylesheets/*.css')
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss())
-		.pipe(gulp.dest('./dist/stylesheets'));
+    gulp.src('./dist/stylesheets/*.css')
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./dist/stylesheets'));
 });
 
 
@@ -108,48 +122,60 @@ gulp.task('move-js-to-dist', function () {
 
 
 gulp.task('prefix-css', ['sass-to-css'], function () {
-	gulp.src('./dist/stylesheets/*.css')
-		.pipe(autoprefixer('last 2 version'))
-		.pipe(gulp.dest('./dist/stylesheets'));
+    gulp.src('./dist/stylesheets/*.css')
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest('./dist/stylesheets'));
 });
 
 
 gulp.task('start-dev', function () {
-	nodemon({
-		script: './bin/www',
-		env: {
-			'NODE_ENV': 'development'
-		}
-	});
+    nodemon({
+        script: './bin/www',
+        env: {
+            NODE_ENV: 'development'
+        }
+    });
 });
 
 
 gulp.task('sass-to-css', function () {
-	gulp.src('./lib/sass/resizive.scss')
-		.pipe(sourcemaps.init())
-		.pipe(sass({style: 'expanded'}))
-		.pipe(sourcemaps.write('./maps'))
-		.pipe(gulp.dest('./dist/stylesheets'));
+    gulp.src('./lib/sass/resizive.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            style: 'expanded'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist/stylesheets'));
 });
 
 
+// gulp.watch('./lib/js/*.js'; ['lint-js']);
+// gulp.watch('./lib/coffee/*.coffee'; ['lint-coffee']);
 gulp.task('watch-files', function () {
-	gulp.watch('./lib/sass/*.scss', ['prefix-css']);
-	// gulp.watch('./lib/js/*.js', ['lint-js']);
-	// gulp.watch('./lib/coffee/*.coffee', ['lint-coffee']);
-	gulp.watch('./lib/js/*.js', notifyLiveReload);
-	gulp.watch('./lib/views/*.jade', notifyLiveReload);
-	gulp.watch('./dist/stylesheets/*.css', notifyLiveReload);
+    gulp.watch('./lib/sass/*.scss', ['prefix-css']);
+    gulp.watch('./lib/js/*.js', notifyLiveReload);
+    gulp.watch('./lib/views/*.jade', notifyLiveReload);
+    gulp.watch('./dist/stylesheets/*.css', notifyLiveReload);
+});
+
+// todo: lint sass
+var verify_tasks = [
+    'lint-js',
+    'lint-js-styles',
+    'lint-coffee'
+];
+gulp.task('verify', verify_tasks, function () {
+
 });
 
 
 var default_tasks = [
-	'prefix-css',
-	'coffee-to-javascript',
-	'move-js-to-dist',
-	'start-dev',
-	'live-reload',
-	'watch-files'
+    'prefix-css',
+    'coffee-to-javascript',
+    'move-js-to-dist',
+    'start-dev',
+    'live-reload',
+    'watch-files'
 ];
 gulp.task('default', default_tasks, function () {
 
@@ -157,15 +183,15 @@ gulp.task('default', default_tasks, function () {
 
 
 var build_tasks = [
-	'minifiy-css',
-	'minify-svgs',
-	'jade-to-html',
-	'lint-js',
-	'move-js-to-dist',
-	'combine-and-minify-js',
-	'lint-coffee',
-	'coffee-to-javascript',
-	'clean-files'
+    'verify',
+    'minifiy-css',
+    'minify-svgs',
+    'jade-to-html',
+    'move-js-to-dist',
+    'combine-and-minify-js',
+    'coffee-to-javascript',
+    'copy-favicon',
+    'clean-files'
 ];
 gulp.task('build', build_tasks, function () {
 
